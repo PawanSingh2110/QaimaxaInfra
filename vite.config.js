f1c9a6b3b038e401e8bdf309dd8dcafd8b873a38
Pawan <singh2110pawan@gmail.com>
2025-05-08 14:08:00 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    historyApiFallback: true
-  }
-})
+    historyApiFallback: true, // Ensures fallback for local dev
+  },
+  build: {
+    outDir: 'dist', // Ensure Vercel is looking for this directory for deployment
+  },
+});
